7ae6a6df481655bd899e2627bfe0a468b829dff3
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-08-13 09:31:30 -0300

    directories, add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
-  return (
+  return (<>
    <h1>Olá mundo</h1>
+   <GlobalStyles />
+   </>
   );
 }
 
